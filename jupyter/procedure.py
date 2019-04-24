2ba99c4ef2270dc970292c27adc1ca23158f42a1
Chris Gong <christophergong@berkeley.edu>
2019-04-24 12:47:16 -0700

    working procedure, need to have general drops

--- jupyter/procedure.py
+++ jupyter/procedure.py
@@ -1,10 +1,31 @@
+import normalization as nrm
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.decomposition import PCA
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.manifold import TSNE
+from sklearn.cluster import KMeans
+from sklearn.mixture import GaussianMixture
+
 def ninetyLocator(var):
+    #locates index over 90
     for i in range(len(var)):
         if var[i] > 90:
             return i
     return -1
 
-def procedure(roi_cluster_df, condition, outputCSV):
+def colorCSV(labels, colors, csvLoc):
+    #outputs the color CSV to the location.
+    results = []
+    for c in labels:
+        results += [colors[int(c)]]
+    df = pd.DataFrame(data={'COLORS': results})
+    df.to_csv(csvLoc, index=False, header=False)
+    return df
+
+def procedure(roi_cluster_df, condition, outputCSV, colors=['#ff0000', '#ffa500', '#ffff00', '#00ff00', '#0000ff', '#551a8b', '#ffc0cb', '#8b4513', '#d3d3d3', '#000000', 'uh', 'oh', 'rip']):
     #use condition
     analysis = []
     if condition == 1:
@@ -22,15 +43,26 @@ def procedure(roi_cluster_df, condition, outputCSV):
     else:
         raise Exception('condition must be 1 through 4.')
 
+    roi_cluster_df = roi_cluster_df.copy() #creates new df
+    colNames = roi_cluster_df.columns[roi_cluster_df.columns.str.contains(pat='z norm')] #keeps only znorm data
+    roi_cluster_df = roi_cluster_df[colNames]
+    roi_cluster_df = roi_cluster_df.drop(['z norm total cell index', 'z norm ind cell index'], axis=1)
+
     labelsNOPCA = []
     labelsPCA = []
-    
-    noanalysis += ['DAPI', 'Tarpg3', 'GLT-1', 'Cav3.1', 'Kv2.2', 'Area', 'Circ','AR','Round','Solidity','stddev', 'median']
-    for col in noanalysis:
-        roi_cluster_df=roi_cluster_df.drop(roi_cluster_df.columns[roi_cluster_df.columns.str.contains(pat=col)], axis=1)
 
+    #use condition and drop/keep cols
     if len(analysis) > 0:
-        roi_cluster_df = roi_cluster_df[analysis]
+        z = []
+        for col in list(roi_cluster_df):
+            for a in analysis:
+                if a in col:
+                    z += [col]
+        roi_cluster_df = roi_cluster_df[z]
+    else:
+        noanalysis += ['DAPI', 'Tarpg3', 'GLT-1', 'Cav3.1', 'Kv2.2', 'Area', 'Circ','AR','Round','Solidity','stddev', 'median']
+        for col in noanalysis:
+            roi_cluster_df = roi_cluster_df.drop(roi_cluster_df.columns[roi_cluster_df.columns.str.contains(pat=col)], axis=1)
 
     roi_cluster_array = roi_cluster_df.iloc[0:roi_cluster_df.shape[0], 0:roi_cluster_df.shape[1]].values
     roi_cluster_array_noPCA = roi_cluster_array.copy()
@@ -38,12 +70,9 @@ def procedure(roi_cluster_df, condition, outputCSV):
     #kmeans and gmm on regular
     kmeans = KMeans(n_clusters=roi_cluster_df.shape[1], random_state = 123) #run on cols
     kmeans.fit(roi_cluster_array_noPCA)
+    gmm = GaussianMixture(n_components=comp).fit(roi_cluster_array_noPCA)
 
-    #roi_clusterResults_df_noPCA=roi_cluster_df.copy()
-    #a=kmeans.labels_
-    #df_a=pd.DataFrame(a)
-    #roi_clusterResults_df_noPCA=pd.concat([roi_clusterResults_df, df_a], axis=1)
-
+    gmm_labelsNOPCA = gmm.predict(roi_cluster_array_noPCA)
     labelsNOPCA = kmeans.labels_
 
     #run pca
@@ -59,14 +88,18 @@ def procedure(roi_cluster_df, condition, outputCSV):
     #kmeans and gmm on pca
     kmeans = KMeans(n_clusters=ncomp, random_state = 123) #make sure this is correct. clusters vs comp
     kmeans.fit(pca_array)
+    gmm = GaussianMixture(n_components=comp).fit(pca_array)
 
-    #roi_clusterResults_df=roi_cluster_df.copy()
-    #a=kmeans.labels_
-    #df_a=pd.DataFrame(a)
-    #roi_clusterResults_df=pd.concat([roi_clusterResults_df, df_a], axis=1)
-
+    gmm_labelsPCA = gmm.predict(pca_array)
     labelsPCA = kmeans.labels_
+    
     #output
+    print('K means PCA: ', labelsPCA)
+    print('K means No PCA: ', labelsNOPCA)
+    print('GMM: ', gmm_labelsPCA)
+    print('GMM No PCA: ', gmm_labelsNOPCA)
 
-    print('PCA: ', labelsPCA)
-    print('No PCA: ', labelsNOPCA)
+    colorCSV(labelsPCA, colors, outputCSV + "K_MEANS_PCA.csv")
+    colorCSV(labelsNOPCA, colors, outputCSV + "K_MEANS_NOPCA.csv")
+    colorCSV(gmm_labelsPCA, colors, outputCSV + "GMM_PCA.csv")
+    colorCSV(gmm_labelsNOPCA, colors, outputCSV + "GMM_NOPCA.csv")
